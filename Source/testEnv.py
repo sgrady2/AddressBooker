6a2d05e5d4340e11c88776fcf7813adb6092914a
sgrady2 <mkkspg@dyn-184-171-35-157.uoregon.edu>
2015-05-11 18:59:54 -0700

    first commit

---
+++ Source/testEnv.py
@@ -0,0 +1,5 @@
+__author__ = 'gregoryhenkhaus'
+
+import validation
+
+print validation.validateSaveEntry(first=False, last='', add1=False, add2='12', city=False, state=False, zip=False, phone=False)
